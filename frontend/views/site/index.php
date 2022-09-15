e63fe4b28ba76f1ed4762be105182b8fc88495f7
JuneAekkalak <aekkalak.ju@gmail.com>
2022-09-15 20:14:23 +0700

    edti title

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,7 +5,7 @@ use app\models\Brand;
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'ElectronicShop';
 $product = Products::find()->where(['status' => '1'])->all();
 $brand = Brand::find()->where(['status' => '1'])->all();
 ?>
